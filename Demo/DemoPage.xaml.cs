9df7253c8275a96c9bd5a3cdac93a6ab248ee52a
Reijnn <Reijnn@gmail.com>
2017-06-20 15:04:39 +0200

    Update Code Style

--- Demo/DemoPage.xaml.cs
+++ Demo/DemoPage.xaml.cs
@@ -4,38 +4,30 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Xamarin.Forms;
 
-namespace Demo
-{
-    public partial class DemoPage : ContentPage
-    {
+namespace Demo {
+    public partial class DemoPage : ContentPage {
         private TrelloItem item { get; set; } = null;
-        public DemoPage()
-        {
+        public DemoPage() {
             InitializeComponent();
         }
 
-		private async Task<bool> getTrelloItem()
-		{
-			try
-			{
-				var json =
-					await new HttpClient().GetStringAsync(
-						"https://api.trello.com/1/board/XUb9AMvz?key=ca4fcd3f80ad1dc2ff1667549030fe15&cards=open&lists=open");
-				item = JsonConvert.DeserializeObject<TrelloItem>(json);
-			}
-			catch (Exception ex)
-			{
-				await DisplayAlert("Error", ex.Message, "Ok");
-				return false;
-			}
-			return true;
-		}
+        private async Task<bool> getTrelloItem() {
+            try {
+                var json = await new HttpClient().GetStringAsync(
+                        "https://api.trello.com/1/board/XUb9AMvz?key=ca4fcd3f80ad1dc2ff1667549030fe15&cards=open&lists=open");
+                item = JsonConvert.DeserializeObject<TrelloItem>(json);
+            } catch (Exception ex) {
+                await DisplayAlert("Error", ex.Message, "Ok");
+                return false;
+            }
+            return true;
+        }
 
-		private async void BGetCards_OnClicked(object sender, EventArgs e)
-		{
-			bGetCards.IsEnabled = false;
-			if (await getTrelloItem()) lvCards.ItemsSource = item.cards;
-			bGetCards.IsEnabled = true;
-		}
+        private async void BGetCards_OnClicked(object sender, EventArgs e) {
+            bGetCards.IsEnabled = false;
+            if (await getTrelloItem())
+                lvCards.ItemsSource = item.cards;
+            bGetCards.IsEnabled = true;
+        }
     }
 }
--- UITests/Tests.cs
+++ UITests/Tests.cs
@@ -5,29 +5,24 @@ using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 
-namespace Demo.UITests
-{
+namespace Demo.UITests {
     [TestFixture(Platform.Android)]
     [TestFixture(Platform.iOS)]
-    public class Tests
-    {
+    public class Tests {
         IApp app;
         Platform platform;
 
-        public Tests(Platform platform)
-        {
+        public Tests(Platform platform) {
             this.platform = platform;
         }
 
         [SetUp]
-        public void BeforeEachTest()
-        {
+        public void BeforeEachTest() {
             app = AppInitializer.StartApp(platform);
         }
 
         [Test]
-        public void TrelloCardsDisplayed()
-        {
+        public void TrelloCardsDisplayed() {
             app.WaitForElement(c => c.Text("Get Cards"));
             app.Tap(c => c.Text("Get Cards"));
             AppResult[] results = app.WaitForElement(c => c.Text("Test Card"));
