c94527303b003f7135a94d18eb0324a48c216f89
Reijnn <Reijnn@gmail.com>
2017-06-20 12:52:14 +0200

    Add Analytics and Crash reporting.

--- Demo/App.xaml.cs
+++ Demo/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace Demo
 {
@@ -13,7 +16,9 @@ namespace Demo
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+			MobileCenter.Start("android=fc4836ed-9aa4-4f47-9467-33bb0c616f61;" +
+                               "ios=6e83aec0-067d-4855-8a7f-58a0b40c27dc;",
+				   typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
